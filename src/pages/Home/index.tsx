4a041b7f6b9fbecb5a0ce6e5adf957a321fc10fe
RafaelPires2 <rafaelbrpires@gmail.com>
2023-01-13 10:30:09 -0300

    feture: home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,22 @@
 import { Header } from "../Header";
 import { Wrapper } from "../Login/styles";
+import { ContainerImageAndText, HomeWrapper } from "./styles";
 
 export function Home() {
   return (
     <>
-      <Header />
       <Wrapper>
-        <h1>Página Home</h1>
+        <Header />
+        <HomeWrapper>
+          <ContainerImageAndText>
+            <div className="conteinerA">
+              <img src="./assets/hospital.svg" alt="" />
+            </div>
+            <div className="conteinerB">
+              <h1>Página Home</h1>
+            </div>
+          </ContainerImageAndText>
+        </HomeWrapper>
       </Wrapper>
     </>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,37 @@
 /* eslint-disable prettier/prettier */
 import styled from 'styled-components'
 
-export const HomeStyled = styled.main``
+export const HomeWrapper = styled.div`
+    width: 120rem;
+    height: calc(100vh - 4.6rem);
+    margin-top: 2rem;
+    
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    background-color: antiquewhite;
+`
+
+export const ContainerImageAndText = styled.section`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 80vh;
+    padding-top: 2rem;
+
+    gap: 2rem;
+
+    background-color: aliceblue;
+    
+    .conteinerA{
+        width: 50rem;
+        background-color: red;
+    }
+
+    .conteinerB{
+        font-size: 3rem;
+        
+    }
+`;
