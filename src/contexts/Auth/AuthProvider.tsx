40556c0ce638dba5824ead6ed2834fec87fe715a
RafaelPires2 <rafaelbrpires@gmail.com>
2023-01-13 13:26:52 -0300

    fix: test

--- src/contexts/Auth/AuthProvider.tsx
+++ src/contexts/Auth/AuthProvider.tsx
@@ -1,50 +1,50 @@
-// import { useEffect, useState } from "react";
-// import { useApi } from "../../hooks/useApi";
-// import { User } from "../../@types/User";
-// import { AuthContext } from "./AuthContext";
+import { useEffect, useState } from "react";
+import { useApi } from "../../hooks/useApi";
+import { User } from "../../@types/User";
+import { AuthContext } from "./AuthContext";
 
-// // eslint-disable-next-line no-undef
-// export const AuthProvider = ({ children }: { children: JSX.Element }) => {
-//   const [user, setUser] = useState<User | null>(null);
-//   const api = useApi();
+// eslint-disable-next-line no-undef
+export const AuthProvider = ({ children }: { children: JSX.Element }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const api = useApi();
 
-//   useEffect(() => {
-//     const validateToken = async () => {
-//       const storageData = localStorage.getItem("authToken");
-//       if (storageData) {
-//         const data = await api.validateToken(storageData);
-//         if (data.user) {
-//           setUser(data.user);
-//         }
-//       }
-//     };
-//     validateToken();
-//   }, [api]);
+  useEffect(() => {
+    const validateToken = async () => {
+      const storageData = localStorage.getItem("authToken");
+      if (storageData) {
+        const data = await api.validateToken(storageData);
+        if (data.user) {
+          setUser(data.user);
+        }
+      }
+    };
+    validateToken();
+  }, [api]);
 
-//   const setToken = (token: string) => {
-//     localStorage.setItem("authToken", token);
-//   };
+  const setToken = (token: string) => {
+    localStorage.setItem("authToken", token);
+  };
 
-//   const signin = async (email: string, password: string) => {
-//     const data = await api.signin(email, password);
-//     if (data.user && data.token) {
-//       console.log(data);
-//       setUser(data.user);
-//       setToken(data.token);
-//       return true;
-//     }
-//     return false;
-//   };
+  const signin = async (email: string, password: string) => {
+    const data = await api.signin(email, password);
+    if (data.user && data.token) {
+      console.log(data);
+      setUser(data.user);
+      setToken(data.token);
+      return true;
+    }
+    return false;
+  };
 
-//   const signout = async () => {
-//     setUser(null);
-//     setToken("");
-//     await api.logout();
-//   };
+  const signout = async () => {
+    setUser(null);
+    setToken("");
+    await api.logout();
+  };
 
-//   return (
-//     <AuthContext.Provider value={{ user, signin, signout }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
+  return (
+    <AuthContext.Provider value={{ user, signin, signout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,30 +1,30 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API,
-// });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API,
+});
 
-// export const useApi = () => ({ 
-//   validateToken: async (token: string) => {
-//     const response = await api.post("/validate", { token });
-//     return response.data;
-//   },
+export const useApi = () => ({ 
+  validateToken: async (token: string) => {
+    const response = await api.post("/validate", { token });
+    return response.data;
+  },
 
-//   signin: async (email: string, password: string) => {
-//     const response = await api.get("/users", { email, password });
-//     const users = await api.get("/users");
-//     const selectedUsers = response.data.filter(
-//       (res) => res.user.email === email && res.user.password === password
-//     );
-//     if (selectedUsers.length > 0) {
-//       selectedUsers[0].user.password = null;
-//       return selectedUsers[0];
-//     }
-//     return selectedUsers;
-//   },
+  signin: async (email: string, password: string) => {
+    const response = await api.get("/users", { email, password });
+    const users = await api.get("/users");
+    const selectedUsers = response.data.filter(
+      (res) => res.user.email === email && res.user.password === password
+    );
+    if (selectedUsers.length > 0) {
+      selectedUsers[0].user.password = null;
+      return selectedUsers[0];
+    }
+    return selectedUsers;
+  },
 
-//   logout: async () => {
-//     const response = await api.post("/logout");
-//     return response.data;
-//   },
-// });
+  logout: async () => {
+    const response = await api.post("/logout");
+    return response.data;
+  },
+});
