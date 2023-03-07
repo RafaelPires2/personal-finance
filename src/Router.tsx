3c27b4be517f0a3d869539568b019b4097fd9862
rafaelpires2 <rafaelbrpires@gmail.com>
2023-03-07 14:52:23 -0300

    feat: auth, api

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,25 @@
 /* eslint-disable prettier/prettier */
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { Login } from './pages/Login'
-import { Dashboard } from './pages/Dashboard'
-import { RequireAuth } from './contexts/Auth/RequireAuth'
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Dashboard } from "./pages/Dashboard";
+import { RequireAuth } from "./contexts/Auth/RequireAuth";
+import { Register } from "./pages/Register";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} />
+      <Route path="/Register" element={<Register />} />
+      <Route
+        path="/dashboard"
+        element={
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        }
+      />
     </Routes>
-  )
+  );
 }
--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -6,21 +6,9 @@ const api = axios.create({
 
 export const useApi = () => ({
   validateToken: async (token: string) => {
-    const response = await api.post("/validate", { token });
+    const response = await api.post("/users", { token });
     return response.data;
   },
-
-  // signin: async (email: string, password: string) => {
-  //   const response = await api.get("/users", { email, password });
-  //   const selectedUsers = response.data.filter(
-  //     (res: any) => res.user.email === email && res.user.password === password
-  //   );
-  //   if (selectedUsers.length > 0) {
-  //     selectedUsers[0].user.password = null;
-  //     return selectedUsers[0];
-  //   }
-  //   return selectedUsers;
-  // },
 
   signin: async (email: string, password: string) => {
     const response = await api.post("/signin", { email, password });
